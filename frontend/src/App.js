742314636b8ed490a61398bb00ecdaccd8a12b67
adarrssh <adarsh00502@gmail.com>
2024-01-07 13:09:56 +0530

    refactor: frontend folder setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
-import { Route } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Home from './Pages/HomePage';
+import { Button, ButtonGroup } from '@chakra-ui/react'
+import HomePage from './Pages/HomePage';
 import ChatPage from './Pages/ChatPage';
 
 function App() {
   return (
     <div className="App">
-      <Route exact path="/" component={Home}/>
-      <Route path="/chats" component={ChatPage}/>
+      <Routes>
+
+        <Route path="/" element={<HomePage/>}/>
+        <Route exact path="/chats" element={<ChatPage/>} />
+      </Routes>
     </div>
   );
 }
--- frontend/src/Pages/ChatPage.js
+++ frontend/src/Pages/ChatPage.js
@@ -1,30 +1,19 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import { useEffect } from 'react'
-
 const ChatPage = () => {
 
     const [chats, setChats] = useState([])
+
     const fetchChats = async () => {
-        try {
-            const {data} = await axios.get('http://localhost:4000/api/chat')
-            setChats(data)
-        } catch (error) {
-            console.log(error)
-        }
+        const data = await axios.get(`${process.env.REACT_APP_BASE_API_URL}/api/chat`)
+        setChats(data)
     }
 
     useEffect(()=>{
         fetchChats()
     },[])
   return (
-    <div>{
-        chats.map(
-            chat =>
-
-            <div key={chat.id}>{chat.chatName}</div>
-            
-            )}</div>
+    <div>ChatPage</div>
   )
 }
 
